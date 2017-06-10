632a5d780e820125948c14e90b58aae6f5a2f5b1
vvardhan <kvvardhanz@gmail.com>
2017-06-10 15:27:56 -0400

    initial checkin

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import * as React from "react";
+import * as ReactDom from "react-dom";
+
+
+import { Home } from "./components/Home";
+
+ReactDOM.render(<Home name="VVardhanz" age ="{29}" />, document.getElementById("app"));
